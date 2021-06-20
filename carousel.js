d385712b0162787249c37d0ef0b111adae87819c
Didoshotev <didoshotev@gmail.com>
2021-06-20 22:30:24 +0300

    style: testimonials and cta sections

---
+++ carousel.js
@@ -0,0 +1,36 @@
+let currentSlide = 1;
+
+const showSlide = (slideIndex) => {
+  const slides = document.getElementsByClassName('carousel-container');
+  if (slideIndex > slides.length) {
+    currentSlide = 1
+  }
+  if (slideIndex < 1) {
+    currentSlide = slides.length
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slides[currentSlide - 1].style.display = 'flex'
+}
+
+
+const nextSlide = () => {
+  showSlide(currentSlide += 1);
+}
+
+const previousSlide = () => {
+  showSlide(currentSlide -= 1);
+}
+
+// window.onload
+document.addEventListener('DOMContentLoaded', () => {
+  showSlide(currentSlide);
+  document.getElementById('prev').addEventListener('click',  () => {
+    previousSlide();
+  })
+
+  document.getElementById('next').addEventListener('click', () => {
+    nextSlide();
+  })
+})
